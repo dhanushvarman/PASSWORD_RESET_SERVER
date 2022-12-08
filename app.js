ed95d756ad3cddcc16e5bc32b81c9c03a12f2ca2
dhanushvarman <dhanushvarmanj@gmail.com>
2022-12-08 15:03:49 +0530

    testing

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ var resetRouter = require('./routes/reset');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
